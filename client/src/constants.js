6f708d4266708093f0b1ceae07d7fb84efab8834
Surya-palanisamy <suryasitit@gmail.com>
2025-02-08 19:55:29 +0530

    live editor 1st upload

--- client/src/constants.js
+++ client/src/constants.js
@@ -5,6 +5,7 @@ export const LANGUAGE_VERSIONS = {
   java: "15.0.2",
   csharp: "6.12.0",
   php: "8.2.3",
+  c: "10.2.0",
 };
 
 export const CODE_SNIPPETS = {
@@ -14,5 +15,5 @@ export const CODE_SNIPPETS = {
   java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
-  php: "<?php\n\n$name = 'Code';\necho $name;\n",
+  php: "<?php\n\n$name = 'Code';\necho $name;\n",c:"\n#include <stdio.h>\n\nint main() {\n\tprintf(\"Hello, World!\");\n\treturn 0;\n}\n",
 };
